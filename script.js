46ab233b51c76500e6e8ed4a6d24e73a40ca5e5a
Rishav Mishra <irishavmishra@proton.me>
2024-06-01 13:04:32 +0530

    Accordion Added

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ menu.addEventListener("click", ()=>{
     navbar.classList.toggle("active")
 })
 
+// Accordion
+
+document.querySelectorAll('.accordion-title').forEach((item) => {
+    item.addEventListener('click', () => {
+        const content = item.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            document.querySelectorAll('.accordion-content').forEach((content) => {
+                content.style.display = 'none';
+            });
+            content.style.display = 'block';
+        }
+    });
+});
